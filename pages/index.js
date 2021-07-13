01e808eee895a9f764b0a04dc85582d0911bc738
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2021-07-13 16:41:01 -0300

    inicio do alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/componentes/MainGrid'
+import Box from '../src/componentes/Box'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+        <img src="https://github.com/rafaelgeomo.png"/>
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem Vindo
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box >
+          Pessoas da Comunidade
+        </Box >
+      </div>
+    </MainGrid>
+  )
 }
